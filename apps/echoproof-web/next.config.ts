ea7c0081418b1700e841876fc8ee5677ee6a1741
Nischaldoescode <nischala389@gmail.com>
2026-05-07 16:57:48 +0530

    added new loaders

--- apps/echoproof-web/next.config.ts
+++ apps/echoproof-web/next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // image config
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+    ],
+  },
+
+  // rewrites (/@username → /user/username)
   async rewrites() {
     return [
       {
